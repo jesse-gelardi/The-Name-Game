4e6767819da86f67562877bae1b85ea975dba142
Jesse Gelardi <jesse.gelardi97@gmail.com>
2022-12-08 14:53:42 -0500

    second commit

---
+++ scripts.js
@@ -0,0 +1,50 @@
+//make a var for the amount of times you have not said your name is "steve"
+//every time after you say your name is not Steve, I want it to repeat
+
+//keep track of users' answers
+
+var userName = window.prompt("Hello! Please enter your name:");
+var nameIsSteve = true; = userName(NaN)
+const nameIsNotSteve = false; 
+
+let nameIsNotSteve = 0; 
+    if (userName = nameIsNotSteve) {
+    nameIsNotSteve = 1; i++;
+    }
+        console.log('firstdialogue')
+    else if (NameIsNotSteve = 2) {
+        console.log('seconddialogue')
+    }
+    else if (NameIsNotSteve = 3) {
+        console.log('thirddialogue')
+    }
+    else if (NameIsNotSteve = 4) {
+        console.log('fourthdialogue')
+    }
+    else if (NameIsNotSteve = 5) {
+        console.log('fifthdialogue')
+    }
+    else if (NameIsNotSteve = 6) {
+        console.log('sixthdialogue')
+    }
+
+
+
+
+for (let nameIsNotSteve = 0; nameIsNotSteve = 1; i++) {
+    console.long("dialogue1here)");
+}
+
+
+
+let NameIsNotSteve = 0;
+var loop = function() {
+    while(NameIsNotSteve=1){
+        ///Your code here!
+        console.log("reponse here");
+        NameIsNotSteve += 1;
+    }
+};
+loop();
+
+  
